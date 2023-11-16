de3484f2fb525ba461d681f28e3fc0fed53e46e9
Lucas Fragalá <lucfragala@gmail.com>
2023-11-15 21:02:59 -0300

    up

---
+++ detalhe.php
@@ -0,0 +1,71 @@
+<div id="corpo-loja">
+    <aside class="banner">
+        <img src="imagens/banner-meio.png">
+    </aside>
+
+    <section class="categorias">
+        <h2 class="fundo_azul">Categorias</h2>
+
+        <nav>
+            <ul class="fundo_azul">
+                <li><a href="">.: Cateegoria 1</a></li>
+                <ul>
+                    <li><a href="">.: Produto 1</a></li>
+                    <li><a href="">.: Produto 2</a></li>
+                    <li><a href="">.: Produto 3</a></li>
+                    <li><a href="">.: Produto 4</a></li>
+                </ul>
+                <li><a href="">.: Cateegoria 2</a></li>
+                <ul>
+                    <li><a href="">.: Produto 1</a></li>
+                    <li><a href="">.: Produto 2</a></li>
+                    <li><a href="">.: Produto 3</a></li>
+                    <li><a href="">.: Produto 4</a></li>
+                </ul>
+                <li><a href="">.: Cateegoria 3</a></li>
+                <ul>
+                    <li><a href="">.: Produto 1</a></li>
+                    <li><a href="">.: Produto 2</a></li>
+                    <li><a href="">.: Produto 3</a></li>
+                    <li><a href="">.: Produto 4</a></li>
+                </ul>
+            </ul>
+        </nav>
+    </section>
+
+    <div id="lado-direito">
+        <section class="vitrine">
+            <div id="cx-img-produto">
+                <a href=""><img src="imagens/css-g.png" alt=""></a>
+            </div>
+
+            <div id="cx-titulo-produto">
+                <h1><a href="">Título</a></h1>
+            </div>
+
+            <div id="cx-preco-produto">
+                <span>Valor:</span><strong>R$37,90</strong>
+            </div>
+
+            <div class="duracao-autor">
+                <h3><span>Duração: </span><strong>12 horas</strong></h3>
+            </div>
+
+            <div class="duracao-autor">
+                <h4><span>Autor: </span><strong>Lucas Fragalá</strong></h4>
+            </div>
+
+            <div id="descricao-rapida">
+                <h2>Descrição Rápida</h2>
+                Lorem Ipsum oajfojeo efoeajfa ffojaoijfa feajfioea foaejfomaf foiaejfea, feoaijfealf aeofjaefaeofa fjaepemfamfl
+            </div>
+
+            <div id="comprar-produto">
+                <form action="">
+                    <input type="submit" value="">
+                </form>
+            </div>
+        </section>
+    </div>
+</div>
+
